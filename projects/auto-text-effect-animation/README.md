0df432b3832f2bfa191b0380b27f670d98255d12
nayem-ahmedz <nayemahmed2n@gmail.com>
2024-06-19 23:13:13 +0600

    Updated files of a Project : auto-text..

---
+++ projects/auto-text-effect-animation/README.md
@@ -0,0 +1,29 @@
+# Project : Auto Text Effect Animation
+
+## Project No: 002
+
+### Project ID: W-HCJ-001
+
+## Overview
+
+**Auto Text Effect Animation** is a dynamic and engaging project that demonstrates how to create text animation effects using HTML, CSS, and JavaScript. This project showcases the power of JavaScript to manipulate the DOM and create interactive web experiences.
+
+## Features
+
+- Dynamic and engaging text animation
+- Easy to customize
+- Responsive design
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Acknowledgements
+
+* created on 18 June 2024
+* version 1.0
+* last updated on 18 June 2024
+
+### Thanks! Have a look at live reload of this site at --
--- projects/auto-text-effect-animation/script.js
+++ projects/auto-text-effect-animation/script.js
@@ -1,13 +1,12 @@
-const job = document.getElementById('job');
-const jobs = ['Web Developer', 'Content Creator', 'Instructor', 'Trainer'];
+const jobs = ['Web Developer', 'Youtuber', 'Instructor'];
+let job = document.getElementById('job');
 
 let jobsIndex = 0;
 let charIndex = 0;
 
-function textAnimate(){
+function updateText(){
     charIndex++;
-    job.innerHTML = `I am ${jobs[jobsIndex].charAt(0) === 'I' ? 'an' : 'a'} ${jobs[jobsIndex].slice(0, charIndex)}`;
-    
+    job.innerHTML = `I am ${jobs[jobsIndex].slice(0,1) === 'I' ? 'an' : 'a'} ${jobs[jobsIndex].slice(0, charIndex)}`;
     if(charIndex === jobs[jobsIndex].length){
         jobsIndex++;
         charIndex = 0;
@@ -15,7 +14,34 @@ function textAnimate(){
     if(jobsIndex === jobs.length){
         jobsIndex = 0;
     }
-    setTimeout(textAnimate, 350);
+    setTimeout(updateText, 300);
 }
 
-textAnimate();
+updateText();
+
+/*
+//javascript Async : learnt today
+//setTimeout : excute after few 1000 miliseconds = 1s
+//syntext : setTimeout(function, miliseconds, params1, params2...);
+
+function printMe(){
+    console.log('Youre Printed ');
+}
+setTimeout(printMe, 2000); //dont use function's parenthesis ()
+
+//with value passing
+function printValue(value){
+    console.log('Youre Printed ' + value);
+}
+setTimeout(printValue, 3000, 'JS');
+
+setTimeout(() =>{
+    console.log('This is Arrow Function');
+}, 4000);
+
+
+//Arow Function
+// () => {
+//     return;
+// }
+*/
