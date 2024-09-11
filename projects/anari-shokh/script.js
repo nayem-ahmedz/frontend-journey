95410e76edd956a822658fa6584ab106fb813919
nayem-ahmedz <nayemahmed2n@gmail.com>
2024-09-11 21:46:35 +0600

    deleted project : anari shoikh

--- projects/anari-shokh/script.js
+++
@@ -1,7 +0,0 @@
-// Nav hamburger icon
-function toggleNav(){
-    let icon = document.querySelector('.hamburger');
-    icon.classList.toggle('h-change');
-    let tpc = document.querySelector('.top-nav-content');
-    tpc.classList.toggle('tnc-toggle');
-}
