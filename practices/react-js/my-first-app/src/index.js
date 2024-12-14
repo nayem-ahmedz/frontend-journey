3ffc1ecf73406b23af1bde8ef7e67497d2d47fad
nayem-ahmedz <nayemahmed2n@gmail.com>
2024-12-15 00:24:33 +0600

    Fixed react env

--- practices/react-js/my-first-app/src/index.js
+++ practices/react-js/my-first-app/src/index.js
@@ -1,29 +1,13 @@
-import React, { useEffect, useState, useReducer, useCallback } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Todos from './Todos';
 
 
 function App(){
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState([]);
-  const increment = () => {
-    setCount((c) => c + 1);
-  }
-  const addTodo = useCallback(() => {
-    setTodos((t) => [...t, 'New Todos']);
-  }, [todos]);
   return(
-    <>
-      <Todos todos={todos} addTodo={addTodo} />
-      <hr />
-      <div>
-        Count : {count}
-        <button onClick={increment}> + </button>
-      </div>
-    </>
+    <h1>Hey</h1>
   );
 }
 
